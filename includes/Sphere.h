4d9d54c4169b677bb1a0b355b86bfc2f380e2b4a
Juan Sebastian Aguirre Zarraonandia <66903979+JSebastianZ@users.noreply.github.com>
2020-06-15 19:59:33 +0900

    Update Sphere.h

--- includes/Sphere.h
+++ includes/Sphere.h
@@ -1,3 +1,12 @@
+/*
+* Interface file for the Ray Tracer Sphere object.
+*
+*
+* This file contains the definitions for the Sphere class,
+* which provides the interfaces to create sphere objects.
+*
+*/
+
 #ifndef SPHERE_H
 #define SPHERE_H
 
@@ -8,13 +17,22 @@
 #include "Object.h"
 
 namespace rtc {
+
+	/*
+	* The Ray Tracer spheres derive from the Object class.
+	*/
+
 	class Sphere : public Object {
 
 	public:
-		//Material m_material;
+
+		// Spheres are defined by their diameter and position within the 3D space.
 		real m_diameter;
 		real m_radius = (m_diameter / 2);
 		Point m_center;
+
+		// Spheres are constructed according to specified parameters.
+		// As objects, a material and transform matrix must be provided upon construction.
 		Sphere(real diameter, Point center, Material material, Matrix transform) : m_diameter{ diameter }, m_center{ center }, Object{ material,transform } {}
 	};
 }
