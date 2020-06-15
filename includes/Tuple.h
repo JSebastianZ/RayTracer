0c71922c739cd10fb0f7439d047330b5cc3763a5
Juan Sebastian Aguirre Zarraonandia <66903979+JSebastianZ@users.noreply.github.com>
2020-06-15 20:22:37 +0900

    Update Tuple.h

--- includes/Tuple.h
+++ includes/Tuple.h
@@ -1,38 +1,53 @@
+/*
+* Interface file for the Ray Tracer tuples.
+*
+*
+* This file contains the definitions for the Tuple class,
+* which is the base class for Points and Vectors.
+*
+*/
+
 #ifndef TUPLE_H
 #define TUPLE_H
-
-
 
 #include "precision.h"
 
 namespace rtc {
 
 	/*
-	Representation of 3D primitives in Cartesian plane
+	* Tuples are used to represent  3D primitives in Cartesian plane (e.g. point, vectors).
 	*/
 
 	class Tuple {
 
 	public:
-		real m_x;	// Holds the value along the x axis.
+		
+		// Holds the value along the x axis.
+		real m_x;	
 
-		real m_y;	// Holds the value along the y axis.
+		// Holds the value along the y axis.
+		real m_y;	
 
-		real m_z;	// Holds the value along the z axis.
+		// Holds the value along the z axis.
+		real m_z;	
 
-		int m_w;	// Indicates primitive type. 0 = vector / 1 = point.
+		// Indicates primitive type. 0 = vector / 1 = point.
+		int m_w;	
 
-	public:
-
+		// Default constructor.
 		Tuple() : m_x(0), m_y(0), m_z(0), m_w(0) {}
 
+		// Constructs a tuple according to parameters passed.
 		Tuple(real x, real y, real z, int w)
 			: m_x{ x }, m_y{ y }, m_z{ z }, m_w{ w }{}
 
+		// Destructor.
 		~Tuple() {}
 
+		// Returns the tuple that results from scaling tuple A by X.
 		static Tuple scalarMultiplication(const Tuple& a, const real& x) {}
 
+		// Returns the tuple that results from dividing tuple A by X.
 		static Tuple scalarDivision(const Tuple& a, const real& x) {}
 	};
 
