cba3b542321b5184f09c2cd2f141cd64951df83a
Juan Sebastian Aguirre Zarraonandia <66903979+JSebastianZ@users.noreply.github.com>
2020-06-15 20:45:46 +0900

    Update World.h

--- includes/World.h
+++ includes/World.h
@@ -1,3 +1,12 @@
+/*
+* Interface file for the Ray Tracer 3D scene.
+*
+*
+* This file contains the definitions for the world class,
+* which defines all the objects to be rendered by the Ray Tracer.
+*
+*/
+
 #ifndef WORLD_H
 #define WORLD_H
 
@@ -9,18 +18,32 @@
 #include "Material.h"
 
 namespace rtc {
+
+	/*
+	* The Ray Tracer renders all the objects defined in this class.
+	*/
+
 	class World {
+
 	public:
 
+		// Every 3D scene must have at least one light source.
 		Light m_light_source;
-		std::vector<Sphere> m_objects;	// Trata de generalizar para que acepte todo tipo de obje
+
+		// World objects are stored in a collection, implemented as a vector.
+		std::vector<Sphere> m_objects;	
+
+		// Definition of objects materials.
 		Material m1 = Material((Color(1, 0.9, 0.9)), 0.1, 0.7, 0, 200.0);
 		Material m4 = Material(Color(0.1, 1, 0.5), 0.1, 0.7, 0.3, 200.0);
 		Material m5 = Material(Color(0.5, 1, 0.1), 0.1, 0.7, 0.3, 200.0);
 		Material m6 = Material(Color(1, 0.8, 0.1), 0.1, 0.7, 0.3, 200.0);
 
+		// Point used as scaling factor.
 		Point p1 = Point(10, 0.01, 10);
 
+		// Definition of each world object transform matrix.
+		// Necessary for every object, at least translate matrix to origin.
 		Matrix t1 = Matrix::multiplyMatrices(Matrix::translation_matrix_p(Point(0,0,0)), Matrix::scaling_matrix_p(p1));
 		Matrix t2 = Matrix::multiplyMatrices(Matrix::translation_matrix_p(Point(0, 0, 5)),
 			(Matrix::multiplyMatrices(Matrix::rotation_matrix_y(-PI / 4),
@@ -32,7 +55,9 @@ namespace rtc {
 		Matrix t5 = Matrix::multiplyMatrices(Matrix::translation_matrix_p(Point(1.5, 0.5, -0.5)), Matrix::scaling_matrix_p(Point(0.5, 0.5, 0.5)));
 		Matrix t6 = Matrix::multiplyMatrices(Matrix::translation_matrix_p(Point(-1.5, 0.33, -0.75)), Matrix::scaling_matrix_p(Point(0.33, 0.33, 0.33)));
 		
-		
+		// 3D scene is created as a World object.
+		// Light sources and objects are constructed.
+		// Objects are stored in an objects collection data structure for reference.
 		World() {
 			Sphere floor(Sphere(2, Point(0, 0, 0), m1, t1));
 			Sphere left_wall(Sphere(2, Point(0, 0, 0), m1, t2));
